fa7e7148ab6bfd1f8df0e6ef380a31819814ea97
Joosep Parts <joosep.parts@gmail.com>
2022-11-27 11:14:50 +0200

    Add files via upload

---
+++ start.bat
@@ -0,0 +1,21 @@
+set mypath=%cd%
+@echo off
+echo.
+echo Active path: %mypath% 
+echo.
+
+rmdir /s /q .\my_index
+mkdir documents
+
+echo.
+echo Made directory "documents". Move files there you want to be indexed.
+echo.
+
+docker pull docker.elastic.co/elasticsearch/elasticsearch:7.14.0 
+docker pull simon987/sist2
+docker-compose up -d
+echo.
+
+start chrome www.0.0.0.0:8080.com
+
+pause
